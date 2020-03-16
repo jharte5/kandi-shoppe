15b240a3d7c60a7249c6876e51068b32d95e4b3f
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-16 18:04:37 -0400

    setting up

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/logout', (req, res) => {
+  req.logout();
+  return res.redirect('/');
+});
+
 module.exports = router;
