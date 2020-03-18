17091a6b5f7a4b35b428aacfcd21e3bfc745865c
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-18 14:29:49 -0400

    gently updated cart controllers and admin routes

---
+++ routes/admin/categories/models/Category.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+    name: {type: String, unique: true, lowercase: true}
+})
---
+++ routes/admin/validation/categoryValidation.js
@@ -0,0 +1,7 @@
+const { check } = require('express-validator');
+const categoryValidation = [
+    check('name', 'Category name cannot be empty').not().isEmpty()
+
+]
+
+module.exports = categoryValidation
