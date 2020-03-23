b5aa1c92b24604a4ec7d1bb90715bbed5309ec93
jharte5 <jonathan.harte@codeimmersives.com>
2020-03-23 10:09:32 -0400

    removed admin routes

--- routes/admin/validation/categoryValidation.js
+++
@@ -1,7 +0,0 @@
-const { check } = require('express-validator');
-const categoryValidation = [
-    check('name', 'Category name cannot be empty').not().isEmpty()
-
-]
-
-module.exports = categoryValidation
